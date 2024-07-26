e53b3a05682e4be282a614572c568768912df0b7
Fakhri Rossi <rossifakhri@gmail.com>
2024-07-26 19:56:46 +0700

    Installed and used react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import "./App.css";
 import Article from "./components/Article";
 import Homepage from "./pages";
 import { GlobalContext } from "./context";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routers";
 
 function App() {
   const user = {
@@ -11,7 +13,8 @@ function App() {
   return (
     <>
       <GlobalContext.Provider value={user}>
-        <Homepage />
+        {/* <Homepage /> */}
+        <RouterProvider router={router} />
       </GlobalContext.Provider>
     </>
   );
