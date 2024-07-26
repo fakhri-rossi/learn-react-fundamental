e62c6e13391b8ed2005e4b4ece9066e24e2166d6
Fakhri Rossi <rossifakhri@gmail.com>
2024-07-26 13:30:38 +0700

    Use state -> reactive variable

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,22 +1,32 @@
+import { useState } from "react";
 import Article from "../components/Article"
 import posts from "../posts.json"
 
 
 export default function Homepage(){
+    const [search, setSearch] = useState("");
+    
+    const changeSearch = (event) => {
+        console.log(event.target.value);
+        setSearch(event.target.value)
+    }
+
     return (
         <>
             <h1>Homepage</h1>
 
             <div>
                 <h3>Cari artikel: </h3>
-                <input type="text" name="" id="" />
+                <input type="text" onChange={ changeSearch } />
             </div>
 
+            <p>text: {search}</p>
+
             {
-                posts.map(({ title, tags, date }) => {
+                posts.map(({ title, tags, date }, index) => {
                     // return <Article title={title} tags={tags} date={date} />
                     // bisa disingkat kek gni:
-                    return <Article { ...{ title, tags, date } } />
+                    return <Article { ...{ title, tags, date } } key={index} />
                 })
             }
         </>
